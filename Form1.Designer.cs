faa7f8b84153bb4ea9a2a3cea492194f1e3913cc
Chandu <Chandu@LAPTOP-FT1QO2GN>
2023-04-10 12:05:15 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Flappy_game
+{
+    partial class lab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pipeButtom = new System.Windows.Forms.PictureBox();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.sky = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sky)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pipeButtom
+            // 
+            this.pipeButtom.BackgroundImage = global::Flappy_game.Properties.Resources.WhatsApp_Image_2023_04_05_at_2_52_00_PM__1_;
+            this.pipeButtom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pipeButtom.Location = new System.Drawing.Point(532, 275);
+            this.pipeButtom.Name = "pipeButtom";
+            this.pipeButtom.Size = new System.Drawing.Size(100, 134);
+            this.pipeButtom.TabIndex = 0;
+            this.pipeButtom.TabStop = false;
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.BackgroundImage = global::Flappy_game.Properties.Resources.WhatsApp_Image_2023_04_05_at_2_52_00_PM__2_;
+            this.flappyBird.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.flappyBird.Location = new System.Drawing.Point(40, 175);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(100, 50);
+            this.flappyBird.TabIndex = 1;
+            this.flappyBird.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.BackgroundImage = global::Flappy_game.Properties.Resources.WhatsApp_Image_2023_04_05_at_2_52_01_PM;
+            this.pipeTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pipeTop.Location = new System.Drawing.Point(331, -1);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(100, 145);
+            this.pipeTop.TabIndex = 2;
+            this.pipeTop.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.BackgroundImage = global::Flappy_game.Properties.Resources.WhatsApp_Image_2023_04_05_at_2_52_00_PM;
+            this.ground.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ground.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ground.Location = new System.Drawing.Point(0, 400);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(800, 50);
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Score:0";
+            // 
+            // sky
+            // 
+            this.sky.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.sky.Image = global::Flappy_game.Properties.Resources.WhatsApp_Image_2023_04_05_at_2_52_00_PM;
+            this.sky.Location = new System.Drawing.Point(1, -1);
+            this.sky.Name = "sky";
+            this.sky.Size = new System.Drawing.Size(820, 22);
+            this.sky.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sky.TabIndex = 5;
+            this.sky.TabStop = false;
+            // 
+            // lab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sky);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.pipeButtom);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MaximizeBox = false;
+            this.Name = "lab";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeButtom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sky)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeButtom;
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox sky;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_game
+{
+    public partial class lab : Form
+    {
+        int pipeSpeed = 8;
+        int gravity = 4;
+        int score = 0;
+    
+        public lab()
+        {
+            InitializeComponent();
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            flappyBird.Top += gravity;
+            pipeButtom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            label1.Text= score.ToString();
+            if(pipeButtom.Left<-50)
+            {
+                pipeButtom.Left = 800;
+                score++;
+            }
+            if (pipeTop.Left < -180)
+            {
+                pipeTop.Left = 550;
+                score++;
+            }
+            if (flappyBird.Bounds.IntersectsWith(pipeButtom.Bounds)||flappyBird.Bounds.IntersectsWith(pipeTop.Bounds)||
+                flappyBird.Bounds.IntersectsWith(ground.Bounds)||flappyBird.Bounds.IntersectsWith(sky.Bounds))
+            {
+                endGame();
+            }
+
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Space)
+            {
+                gravity= -4;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 4;
+            }
+        }
+        private void endGame()
+        {
+            gameTimer.Stop();
+            label1.Text += "  Game over";
+
+        }
+    }
+}
